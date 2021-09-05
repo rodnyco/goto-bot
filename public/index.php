717db334428188035bab2b2a920b7d5eb0cd7a25
Владимир Раду <rodny_co@list.ru>
2021-09-05 10:03:29 +0300

    RewriteEngine on

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 require '../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 
+echo 'test';
+
 $app = AppFactory::create();
 $app->get('/hello', function ($request, $response, array $args) {
     echo "hello";
