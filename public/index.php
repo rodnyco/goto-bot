22358a927bfb4095cb586cc704da203b3ebf93ea
Владимир Раду <rodny_co@list.ru>
2021-09-07 11:35:37 +0300

    StartCommand

--- public/index.php
+++ public/index.php
@@ -1,72 +1,6 @@
 <?php
 require '../vendor/autoload.php';
 
-use Longman\TelegramBot\Entities\Update;
-use Longman\TelegramBot\Request;
-use Slim\Factory\AppFactory;
-//
-//$dbConfig = [
-//    'host' => 'mysql',
-//    'name' => 'main',
-//    'user' => 'root',
-//    'pass' => 'root',
-//    'port' => '3306',
-//];
-//
-//$dsn = sprintf(
-//    'mysql:host=%s;dbname=%s;port=%s;charset=utf8',
-//    $dbConfig['host'],
-//    $dbConfig['name'],
-//    $dbConfig['port']
-//);
-//
-//$pdo = new PDO($dsn, $dbConfig['user'], $dbConfig['pass']);
-//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-//
-//$query = "SELECT * from `places`";
-//$st = $pdo->prepare($query);
-//$st->execute();
-//$res = $st->fetchAll();
-//echo "<pre>";
-//print_r($res);
-//echo "</pre>";
-
-$url = getenv('JAWSDB_MARIA_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-
-$settings = require __DIR__ . '/../src/App/Settings.php';
-$hostname = $settings['db']['host'];
-$username = $settings['db']['user'];
-$password = $settings['db']['pass'];
-$database = $settings['db']['name'];
-
-try {
-    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    //echo "Connected successfully";
-}  catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
-//$query = "SELECT * from `places`";
-//$st = $conn->prepare($query);
-//$st->execute();
-//while ($res = $st->fetch()) {
-//    echo "<pre>";
-//    print_r($res);
-//    echo "</pre>";
-//}
-
-
 $bot_api_key  = getenv('TG_API_KEY');;
 $bot_username = 'goto_story_bot';
 
@@ -74,40 +8,10 @@ try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    // Handle telegram webhook request
+    $telegram->addCommandClass(StartCommand::class);
     $telegram->handle();
-
-    $post = json_decode(Request::getInput(), true);
-
-
-    $oUpdate = new Update($post, $bot_username);
-    $oMessage = $oUpdate->getMessage();
-
-    $sText = $oMessage->getText();
-
-    file_put_contents("php://stderr", print_r($oMessage->getChat()->getId(), true));
-    $result = Request::sendMessage([
-        'chat_id' => $oMessage->getChat()->getId(),
-        'text'    => $sText,
-    ]);
-
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
     // log telegram errors
     //echo $e->getMessage();
 }
-
-
-$app = AppFactory::create();
-$app->get('/hello', function ($request, $response, array $args) {
-    echo "hello";
-});
-
-
-
-try {
-    $app->run();
-} catch (Exception $e) {
-
-}
-
---
+++ src/Command/StartCommand.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+
+class StartCommand extends \Longman\TelegramBot\Commands\SystemCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'start';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Start command';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/start';
+
+    /**
+     * @var string
+     */
+    protected $version = '0.0.1';
+
+    /**
+     * @var bool
+     */
+    protected $private_only = true;
+
+    /**
+     * Main command execution
+     *
+     * @return \Longman\TelegramBot\Entities\ServerResponse
+     * @throws TelegramException
+     */
+    public function execute(): \Longman\TelegramBot\Entities\ServerResponse
+    {
+        // If you use deep-linking, get the parameter like this:
+        // $deep_linking_parameter = $this->getMessage()->getText(true);
+
+        return $this->replyToChat(
+            'Привет! Этот бот поможет тебя сохранять места, в которые обязательно нужно сходить' . PHP_EOL .
+            'Type /help to see all commands!'
+        );
+    }
+}
